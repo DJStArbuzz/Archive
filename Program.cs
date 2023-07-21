9457a73f3dbc91db8ed09b2fa2c822b972aeab6f
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-07-21 18:54:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Program_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            Console.WriteLine(a + b);
+        }
+
+    }
+}
