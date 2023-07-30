5a3aaffa775f9b0a2e37918a07e11082fca7ac37
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-07-30 21:46:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-namespace Program_1
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            int a = 1;
-            int b = 2;
-            Console.WriteLine(a + b);
-        }
+using System;
+using System.Net.NetworkInformation;
 
+namespace BuildASentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("dsada", 1);
+            dict.Add("asdsa", 2);
+            Dictionary<string, int>.KeyCollection keys = dict.Keys;
+            Console.WriteLine(dict["dsada"]);
+            List<int> numList = new List<int> { 1, 2, 3 };
+            for (int i = 0; i < numList.Count; i++)
+            {
+                Console.WriteLine(numList[i]);
+            }
+            List<string> sp = new List<string> { "red", "orange", "yellow" };
+            string[] sp_2 = { "red", "green", "yellow"};
+            HashSet<string> combo = new HashSet<string>(sp_2);
+            combo.SymmetricExceptWith(sp);
+            foreach (string s in combo)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
     }
 }
