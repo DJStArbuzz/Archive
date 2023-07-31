bd3fbe01d7667f7b0c93800da7aedc6b3855e3dc
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-07-31 22:01:40 +0300

    Create factorial.cs

---
+++ factorial.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace FactorialException
+{
+    public class MyMathFunctions
+    {
+        public static int Factorial(int value)
+        {
+            if (value < 0)
+            {
+                string s = String.Format ( "Отрицательный аргумент в вызове Factorial {0}", value);
+                throw new ArgumentException(s);
+            }
+            int factorial = 1;
+            do
+            {
+                factorial *= value;
+            }
+            while (--value > 1);
+            return factorial;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 6; i > -6; i--)
+                {
+                    int factorial = MyMathFunctions.Factorial(i);
+                    Console.WriteLine("i = {0}, факториал = {1}", i, factorial);
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Фатальная ошибка");
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("Конец");
+        }
+    }
+}
