a72e4d0e1cf52e5af76779678937db8fcc80d7e1
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-08-02 16:51:18 +0300

    Create claymoreBeta.cs

---
+++ project/claymoreBeta.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health;
+            int armor;
+            int damage;
+
+            Console.Write("Количество здоровья: ");
+            health = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Количество здоровья: ");
+            armor = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Количество здоровья: ");
+            damage = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
