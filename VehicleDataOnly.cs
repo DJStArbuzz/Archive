f0443cac7eee29a6a0149a1ebb54a807af0c9329
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2023-08-04 15:46:17 +0300

    Update VehicleDataOnly.cs

--- VehicleDataOnly.cs
+++ VehicleDataOnly.cs
@@ -2,62 +2,37 @@ using System;
 
 namespace VehicleDataOnly
 {
+    public class Motor
+    {
+        public int power;           // Мощность
+        public double displacement; // Рабочий объем
+    }
+
     public class Vehicle
     {
         public string model;        // Название модели 
         public string manufacturer; // Производитель 
         public int numOfDoors;      // Количество дверей 
         public int numOfWheels;     // Количество колес 
+        public Motor motor;
     }
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите информацию о машине.");
-            Console.WriteLine("Неправильная информация будет изменяться автоматически.");
-            // Создаем экземпляр
-            Vehicle my_car = new Vehicle();
-            // Вносим данные для экземляра my_car
-            int num_of_doors, num_of_wheels;
-            string model_name, manufacturer;
+            Motor largeMotor = new Motor();
+            largeMotor.power = 230;
+            largeMotor.displacement = 4.0;
 
-            Console.Write("Модель - ");
-            model_name = Console.ReadLine();
+            Vehicle sonsCar = new Vehicle();
+            sonsCar.model = "Cherokee Sport";
+            sonsCar.manufacturer = "Jeep";
+            sonsCar.numOfDoors = 2;
+            sonsCar.numOfWheels = 4;
+            sonsCar.motor = largeMotor;
 
-            Console.Write("Производитель - ");
-            manufacturer = Console.ReadLine();
-
-            Console.Write("Количество дверей - ");
-            try {
-                num_of_doors = Convert.ToInt32(Console.ReadLine());
-            }
-            catch
-            {
-                Console.WriteLine("Вы вписали не число");
-                num_of_doors = 4;
-            }
-
-            Console.Write("Количество колес - ");
-            try
-            {
-                num_of_wheels = Convert.ToInt32(Console.ReadLine());
-            }
-            catch
-            {
-                Console.WriteLine("Вы вписали не число");
-                num_of_wheels = 4;
-            }
-
-            my_car.model = model_name;
-            my_car.manufacturer = manufacturer;
-            my_car.numOfDoors = num_of_doors;
-            my_car.numOfWheels = num_of_wheels;
-            // Вывод информации
-            Console.WriteLine("\nВаша машина:");
-            Console.WriteLine($"{my_car.manufacturer} {my_car.model}");
-            Console.WriteLine($"С {my_car.numOfDoors} дверями на {my_car.numOfWheels} колесах");
-            Console.WriteLine("Конец программы.");
-            
+            Console.WriteLine("Рабочий объем равен " + sonsCar.motor.displacement); 
         }
     }
 
